2ff410a94f6e98f2dbb12948c5beb3242d3d5317
waremad <mycarline5@gmail.com>
2026-04-29 11:06:51 +0900

    1

--- memeroid.py
+++ memeroid.py
@@ -1,1 +1,15 @@
-#
+import os
+
+dir_path = "default"
+
+chafiles = [f for f in os.listdir(dir_path+"/cha") if f.endswith(".wav")]
+worfiles = [f for f in os.listdir(dir_path+"/wor") if f.endswith(".wav")]
+
+for i in range(len(chafiles)):
+    chafiles[i] = chafiles[i][:-4]
+for i in range(len(worfiles)):
+    worfiles[i] = worfiles[i][:-4]
+
+print(chafiles,worfiles)
+
+goal = input("??")
